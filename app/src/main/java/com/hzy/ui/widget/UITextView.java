b6dcfa9ca7bf43a53eb9822f5dbe2435cc59f7bf
hanzhenyu <zhneyu.han@aliyun.com>
2020-11-03 00:13:31 +0800

    init

---
+++ app/src/main/java/com/hzy/ui/widget/UITextView.java
@@ -0,0 +1,27 @@
+package com.hzy.ui.widget;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class UITextView extends AppCompatTextView {
+
+    public UITextView(@NonNull Context context) {
+        super(context);
+    }
+
+    public UITextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UITextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
